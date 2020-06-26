a810f8b7c21edea01d679c84d43619957ae686d0
zzq <1546374673@qq.com>
2020-06-26 10:06:54 +0800

    省市区三级联动修改

---
+++ wheelview/src/main/java/com/contrarywind/listener/ISelectTimeCallback.java
@@ -0,0 +1,10 @@
+package com.contrarywind.listener;
+
+/**
+ * Created by zengsong on 2018/3/21.
+ */
+
+public interface ISelectTimeCallback {
+
+    public void onTimeSelectChanged();
+}
---
+++ wheelview/src/main/java/com/contrarywind/listener/OnDismissListener.java
@@ -0,0 +1,8 @@
+package com.contrarywind.listener;
+
+/**
+ * Created by Sai on 15/8/9.
+ */
+public interface OnDismissListener {
+    public void onDismiss(Object o);
+}
